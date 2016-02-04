4feab311c58936ed26a35de785c9ea6d01efd8f7
simplesphere <anoop.jeewoolall@gmail.com>
2016-02-04 16:58:09 -0500

    ejs template

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express on Azure' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
+});
+
+module.exports = router;
